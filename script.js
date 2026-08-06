7eed2646c276b5514d43210c85f9495bca00536a
shawsachi0000-creator <shawsachi0000@gmail.com>
2026-08-06 15:17:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,3 +112,20 @@ viewer.style.display="none";
 }
 
 };
+// Loading Screen
+
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+const loader=document.getElementById("loader");
+
+if(loader){
+
+loader.style.display="none";
+
+}
+
+},2000);
+
+});
